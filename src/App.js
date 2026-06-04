4e2fdf2dda8daf91b0f8866b64ebc10316fc451b
vineethvijaych <vineethcpz6881.com>
2026-06-04 16:24:14 +0530

    farmer motion installed properly

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
